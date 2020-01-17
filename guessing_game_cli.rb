0d47db78179da1718e544a5e84577c01f0feee56
Jack Overby <jroverby92@gmail.com>
2020-01-17 18:22:34 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,4 +2,9 @@ def run_guessing_game
   secret = Random.new.rand(1..6)
   print "Enter a number between 1 and 6: "
   case (gets.chomp)
-    when 
+    when secret
+      puts "You guessed the correct number!"
+    when "exit"
+      puts "Goodbye!"
+    else
+      puts "Sorry! The computer guessed #{secret}."
